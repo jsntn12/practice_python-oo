90ffa789c1ec980e12f9af81cb82f74fbf1a6092
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-04-30 09:40:32 -0400

    Completed practice exercise

--- serial.py
+++ serial.py
@@ -20,3 +20,18 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=0):
+        self.start = start
+        self.count = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start= {self.start} next= {self.count + 1}>"
+
+    def reset(self):
+        """Resest the counter to orginal start value."""
+        self.count = self.start
+
+    def generate(self):
+        """Return the next serial."""
+        self.count += 1
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,17 @@
 """Word Finder: finds random words from a dictionary."""
+from random import choice
 
 
 class WordFinder:
-    ...
+    """Machine for finding random word from a file"""
+
+    def __init__(self, file):
+        self.file = file
+        with open(self.file) as self.f:
+            self.wordlist = [word for word in self.f.readlines() if len(
+                word) > 1 and not word.startswith("#")]
+        print(f"{len(self.wordlist)}words read")
+
+    def random(self):
+        """Return a random word"""
+        return choice(self.wordlist)
